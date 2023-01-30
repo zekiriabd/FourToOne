3beae555474c400bcc6ecbffd68cc8e14c06f8b6
Abderahmane Aoufi <Abdouou7@gmail.com>
2023-01-30 22:10:22 +0100

    restructure and add demo3

--- React/Demo2/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Article from "./Article";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Article />
-  </React.StrictMode>
-);
